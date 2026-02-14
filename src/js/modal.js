876b3dc1cc523b54868e5aecf92e8c4ee53e872f
luay <luay@keen-on.de>
2026-02-15 00:28:23 +0100

    adjust-works-navy

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,7 @@
 const startPage = document.querySelector(".start-page");
 const worksNavy = document.querySelector(".works-navy");
+const worksNavyCon = worksNavy.querySelector(".container");
+const worksNavyItems = worksNavyCon.querySelectorAll("div");
 const navItems = document.querySelectorAll(".start-page .list-con div");
 const imageCon = document.querySelector(".image-con");
 navItems.forEach((item) => {
@@ -8,7 +10,21 @@ navItems.forEach((item) => {
     imageCon.classList.add("fade-out");
     setTimeout(() => {
       startPage.classList.add("d-none");
-      worksNavy.classList.remove("d-none");
+      if (item.dataset.section === "works") {
+        worksNavy.classList.remove("d-none");
+      }
     }, 4000);
+  });
+});
+
+worksNavyItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    worksNavyCon.classList.add("navy-pressed")
+    worksNavy.style.cssText += `
+        height:4rem;
+        background-color:transparent;
+        inset: unset;
+        bottom:0;
+        `;
   });
 });

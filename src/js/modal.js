e26146a15868ac1a75b73e9efe87a01cbd6a882a
luay <luay@keen-on.de>
2026-02-15 01:09:33 +0100

    adjust-header

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,25 +1,38 @@
+const header = document.querySelector("header");
+const navItems = header.querySelectorAll(".container div");
 const startPage = document.querySelector(".start-page");
 const worksNavy = document.querySelector(".works-navy");
 const worksNavyCon = worksNavy.querySelector(".container");
 const worksNavyItems = worksNavyCon.querySelectorAll("div");
-const navItems = document.querySelectorAll(".start-page .list-con div");
+const startPageNavItems = document.querySelectorAll(
+  ".start-page .list-con div",
+);
 const imageCon = document.querySelector(".image-con");
-navItems.forEach((item) => {
+startPageNavItems.forEach((item) => {
   item.addEventListener("click", function () {
-    navItems.forEach((item) => item.classList.add("fade-out"));
+    startPageNavItems.forEach((item) => item.classList.add("fade-out"));
     imageCon.classList.add("fade-out");
     setTimeout(() => {
       startPage.classList.add("d-none");
       if (item.dataset.section === "works") {
         worksNavy.classList.remove("d-none");
+      } else {
+        header.classList.remove("d-none");
       }
+      navItems.forEach((navItem) => {
+        navItem.classList.remove("active");
+        navItem.id === item.dataset.section
+          ? navItem.classList.add("active")
+          : navItem.classList.remove("active");
+      });
     }, 4000);
   });
 });
 
 worksNavyItems.forEach((item) => {
   item.addEventListener("click", function () {
-    worksNavyCon.classList.add("navy-pressed")
+    worksNavyCon.classList.add("navy-pressed");
+    header.classList.remove("d-none");
     worksNavy.style.cssText += `
         height:4rem;
         background-color:transparent;

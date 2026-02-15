45009b40b85c897c4fe6c27b6cdac6c931d2bb7e
luay <luay@keen-on.de>
2026-02-15 21:45:40 +0100

    adjust-vite-2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  base: "/",
+  base: "/portfolio",
   root: "./src",
   build: {
     outDir: "../dist",
